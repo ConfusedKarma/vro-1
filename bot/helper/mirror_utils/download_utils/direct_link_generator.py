9ae5647170e0c29afc058e69badd34f036ea7d43
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-01-06 22:15:03 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -83,6 +83,8 @@ def direct_link_generator(link: str):
         return try2link(link)
     elif 'ez4short.com' in link:
         return ez4(link)
+    elif 'send.cm' in link:
+        return sendcm(link)
     # shareus
     elif "https://shareus.in/" in link:
         return shareus(link)
@@ -760,3 +762,25 @@ def gofile(url:str) -> str:
         download_url= content["link"]
        
         return download_url
+
+def sendcm(url:str) -> str:
+    
+    base_url = "https://send.cm/"
+    client = requests.Session()
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+    }
+    
+    response  = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
+    
+    inputs = soup.find_all("input")
+    file_id = inputs[1]["value"]
+    file_name = re.findall("URL=(.*?) - ", response)[0].split("]")[1]
+    parse = {"op": "download2", "id": file_id, "referer": url}
+    
+    resp = client.post(base_url, data=parse, headers=headers, allow_redirects=False)
+    download_url = resp.headers["Location"]
+    
+    return download_url 
