149ab3f2d1b21a128c2cf2bc0f3d67497a9b5617
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-01-11 22:45:29 +0530

    Update misc.py

--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -286,3 +286,51 @@ async def mediainfo(client, message: Message):
     	       return await ddl_mediainfo(client, message, url=user_url)
     	      	
     return await message.reply_text("This type of URL is not supported.", quote=True)
+
+import os
+
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+# View Structure Telegram Message As JSON
+@app.on_message(filters.command(["json"], CUSTOM_CMD))
+async def jsonify(_, message):
+    the_real_message = None
+    reply_to_id = None
+
+    the_real_message = message.reply_to_message or message
+    try:
+        await message.reply_text(
+            f"<code>{the_real_message}</code>",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="❌ Close",
+                            callback_data=f"close#{message.from_user.id}",
+                        )
+                    ]
+                ]
+            ),
+        )
+    except Exception as e:
+        with open("json.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(the_real_message))
+        await message.reply_document(
+            document="json.text",
+            caption=f"<code>{str(e)}</code>",
+            disable_notification=True,
+            reply_to_message_id=reply_to_id,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="❌ Close",
+                            callback_data=f"close#{message.from_user.id}",
+                        )
+                    ]
+                ]
+            ),
+        )
+        os.remove("json.text")
