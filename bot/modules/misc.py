b31fd2760480c5e4b3bf21e684ea98941b074e63
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-01-11 19:46:41 +0530

    Update misc.py

--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -261,11 +261,11 @@ async def telegram_mediainfo(client,message):
         await reply_msg.delete()
         await message.reply_text(f"Something went wrong while generating Mediainfo of replied Telegram file.", quote=True)
         
-CUSTOM_CMD = ["/", "!"]
+#CUSTOM_CMD = ["/", "!"]
 
 mediainfo_usage = f"**Generate mediainfo from Google Drive Links, Telegram files or direct download links. Reply to any telegram file or just pass the link after the command."
 
-@app.on_message(filters.command("m", CUSTOM_CMD))
+@app.on_message(filters.command("/m")#, CUSTOM_CMD))
 @authorized_chats
 async def mediainfo(client, message: Message):
      
