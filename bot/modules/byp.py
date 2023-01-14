2eeae4c43062849b425ba67d05aff323a4f2fde4
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-01-14 13:06:55 +0530

    Update byp.py

--- bot/modules/byp.py
+++ bot/modules/byp.py
@@ -8,7 +8,7 @@ CUSTOM_CMD = ["/", "!"]
 @vro.on_message(filters.command("byp", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+") & filters.user(OWNER_ID))
 async def byp(bot, update):
     url = update.matches[0].group(0)
-    bsdk = shareus(url)
+    bsdk = uptobox(url)
     message = await update.reply_text(
         text=bsdk, disable_web_page_preview=True, quote=True
     )
