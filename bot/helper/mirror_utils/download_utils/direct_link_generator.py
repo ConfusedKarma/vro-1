703c49f6eb6c6154c2812be89f71755c2e7e9c10
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-01-20 19:00:59 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -90,6 +90,8 @@ def direct_link_generator(link: str):
         return shareus(link)
     elif 'mdisk.me' in link:
         return mdisk(link)
+    elif 'gplinks.co' in link:
+        return gplinks(link)
     elif 'gofile.io' in link:
         return gofile(link)
     elif any(x in link for x in fmed_list):
@@ -784,3 +786,29 @@ def sendcm(url:str) -> str:
     download_url = resp.headers["Location"]
     
     return download_url 
+
+def gplinks(url: str) -> str:
+	
+	url = url[:-1] if url[-1] == '/' else url
+	token = url.split("/")[-1]
+	
+	domain ="https://gplinks.co/"
+	referer = "https://mynewsmedia.co/"
+
+	
+	client = requests.Session()
+	vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
+	url = f"{url}/?{vid}"
+
+	response = client.get(url, allow_redirects=False)
+	soup = BeautifulSoup(response.content, "html.parser")
+	
+	
+	inputs = soup.find(id="go-link").find_all(name="input")
+	data = { input.get('name'): input.get('value') for input in inputs }
+	
+
+	time.sleep(5)
+	headers={"x-requested-with": "XMLHttpRequest"}
+	bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
+	return bypassed_url
