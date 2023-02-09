77be95c27c97f5cc1147f5245cabd784fb4dd64e
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-02-09 19:51:11 +0530

    Update misc.py

--- bot/modules/misc.py
+++ bot/modules/misc.py
@@ -50,12 +50,3 @@ async def jsonify(_, message):
             ),
         )
         os.remove("json.text")
-
-
-@vro.on_message(filters.incoming & filters.private)
-async def frwd(message):
-  try:
-    USER_ID = message.from_user.mention
-    await vro.forward_message(message.from_user.id, OWNER_ID)
-  except:
-    vro.send_message(OWNER_ID, f"{USER_ID} is spamming me! I am not able to forward his messages")
