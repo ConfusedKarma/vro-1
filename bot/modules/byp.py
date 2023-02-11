d2a4f938f9181d5de4a78d22f1899fd70161fb1b
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-02-11 21:41:41 +0530

    Update byp.py

--- bot/modules/byp.py
+++ bot/modules/byp.py
@@ -2,10 +2,11 @@ from bot import vro, OWNER_ID
 from bot.helper.mirror_utils.download_utils.direct_link_generator import *
 from pyrogram import filters
 import regex as re
-
+from bot.helper.ext_utils.bot_utils import authorized_chats
 CUSTOM_CMD = ["/", "!"]
 
-@vro.on_message(filters.command("byp", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+") & filters.user(OWNER_ID))
+@vro.on_message(filters.command("byp", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+"))
+@authorized_chats
 async def byp(bot, update):
     url = update.matches[0].group(0)
     bsdk = xpshort(url)
@@ -13,10 +14,20 @@ async def byp(bot, update):
         text=bsdk, disable_web_page_preview=True, quote=True
     )
 
-@vro.on_message(filters.command("t", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+") & filters.user(OWNER_ID))
+@vro.on_message(filters.command("t", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+"))
+@authorized_chats
 async def byp(bot, update):
     url = update.matches[0].group(0)
     bsdk = terabox(url)
+    message = await update.reply_text(
+        text=bsdk, disable_web_page_preview=True, quote=True
+    )
+
+@vro.on_message(filters.command("rc", CUSTOM_CMD) & filters.regex(r"https?://[^\s]+"))
+@authorized_chats
+async def byp(bot, update):
+    url = update.matches[0].group(0)
+    bsdk = rock(url)
     message = await update.reply_text(
         text=bsdk, disable_web_page_preview=True, quote=True
     )
