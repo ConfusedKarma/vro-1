c96c2106dd76f28c9d8ee9ad62133a295924bdac
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-02-17 21:44:10 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -76,7 +76,7 @@ def direct_link_generator(link: str):
         return filepress(link)
     elif is_sharer_scraper(link):
         return sharer(link)
-    elif any(x in link for x in ['terabox.com', 'mirrobox.com', '4funbox.com', 'nephobox.com']):
+    elif any(x in link for x in ['terabox', 'nephobox', '4funbox', 'mirrobox', 'momerybox', 'teraboxapp']):
         return terabox(link)
     elif 'rocklinks.net' in link:
         return rock(link)
