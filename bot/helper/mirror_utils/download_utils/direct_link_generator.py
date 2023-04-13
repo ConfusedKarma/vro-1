ca44e98d3d329b6379fe3c9e58b90674e864aa74
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2023-04-13 12:08:05 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -777,30 +777,21 @@ def sendcm(url:str) -> str:
     return download_url 
 
 def gplinks(url: str):
- 
- url = url[:-1] if url[-1] == '/' else url
- token = url.split("/")[-1]
-    
- domain ="https://gplinks.co/"
- referer = "https://mynewsmedia.co/"
-
-    
- client = requests.Session()
- vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
- url = f"{url}/?{vid}"
-
- response = client.get(url, allow_redirects=False)
- soup = BeautifulSoup(response.content, "html.parser")
-    
-    
- inputs = soup.find(id="go-link").find_all(name="input")
- data = { input.get('name'): input.get('value') for input in inputs }
-    
-
- sleep(5)
- headers={"x-requested-with": "XMLHttpRequest"}
- bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
- return bypassed_url
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    token = url.split("/")[-1]
+    domain ="https://gplinks.co/"
+    referer = "https://mynewsmedia.co/"
+    vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
+    url = f"{url}/?{vid}"
+    response = client.get(url, allow_redirects=False)
+    soup = BeautifulSoup(response.content, "html.parser")
+    inputs = soup.find(id="go-link").find_all(name="input")
+    data = { input.get('name'): input.get('value') for input in inputs }
+    sleep(10)
+    headers={"x-requested-with": "XMLHttpRequest"}
+    bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
+    try: return bypassed_url
+    except: return 'Something went wrong :('
 
 def filepress(url):
     cget = create_scraper().request
